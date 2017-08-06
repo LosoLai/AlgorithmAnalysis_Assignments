fd94395db8007f03c10149232e2200812424522b
rmit-s3579161-loso-lai <s3579161@student.rmit.edu.au>
2017-08-06 15:08:37 +1000

    Modified the add method. AddLast(a bit like queue: Add first Print first)

--- src/LinkedListMultiset.java
+++ src/LinkedListMultiset.java
@@ -45,9 +45,9 @@ public class LinkedListMultiset<T> extends Multiset<T>
         }
         // otherwise, add node to the head of list.
         else {
-            newNode.setNext(mHead);
-            mHead.setPrev(newNode);
-            mHead = newNode;
+        	mTail.setNext(newNode);
+            newNode.setPrev(mTail);
+            mTail = newNode;
         }
         
         mLength++;
