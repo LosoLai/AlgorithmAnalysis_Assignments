67cfd4134b1563a1cb31f7c68f7c20ce7c58d997
s3579161 <s3579161@W8417986.rmit.internal>
2017-08-26 14:59:59 +1000

    modified file recording part

--- src/PerformanceAnalysis.java
+++ src/PerformanceAnalysis.java
@@ -46,31 +46,8 @@ public class PerformanceAnalysis
             
             // data structure to be used
             String dataUsed = args[0];
-            
-            // figure out which data structure we are using    		
             Multiset<String> implementType = null;
-            switch(dataUsed) {
-	            case "linkedlist":
-	            	implementType = new LinkedListMultiset<String>();
-					break;
-				case "sortedlinkedlist":
-					implementType = new SortedLinkedListMultiset<String>();
-					break;
-				case "bst":
-					implementType = new BstMultiset<String>();
-					break;
-				case "hash":
-					implementType = new HashMultiset<String>();
-					break;
-				case "baltree":
-					implementType = new BalTreeMultiset<String>();
-					break;
-				default:
-					System.err.println("Unknown implmementation type.");
-                    printUsage(progName);
-                    System.exit(1);
-            }
-            
+                        
             //args[1] = fix data size
             //args[2] = operation size
             String title = "FixData" + args[1] + "_OpSize" + args[2];
@@ -84,40 +61,73 @@ public class PerformanceAnalysis
             long endTime = 0;
             double timeElapsed = 0;
             // record 10 times experiments
-            long sum = 0;
+            double[] recordList = new double[EXP_NUMBER];
             
-            PrintWriter searchOutWriter = new PrintWriter(recordFileName);
+            PrintWriter filehOutWriter = new PrintWriter(recordFileName);
             for(int i=0 ; i<dataOrder.length ; i++)
             {
+            	// figure out which data structure we are using    		
+	            implementType = null;
+	            switch(dataUsed) {
+		            case "linkedlist":
+		            	implementType = new LinkedListMultiset<String>();
+						break;
+					case "sortedlinkedlist":
+						implementType = new SortedLinkedListMultiset<String>();
+						break;
+					case "bst":
+						implementType = new BstMultiset<String>();
+						break;
+					case "hash":
+						implementType = new HashMultiset<String>();
+						break;
+					case "baltree":
+						implementType = new BalTreeMultiset<String>();
+						break;
+					default:
+						System.err.println("Unknown implmementation type.");
+	                    printUsage(progName);
+	                    System.exit(1);
+	            }
+	            
             	for(int j=1 ; j<=testingCases ; j++)
-                {
+                {    	            
             		fileName = title + "_Test" + Integer.toString(j) + "_" + dataOrder[i] + ".txt";
             		
             		try {
             			FileReader in = new FileReader(new File(fileName));
             		    BufferedReader inReader = new BufferedReader(in);
-            			//PrintWriter searchOutWriter = new PrintWriter(System.out, true);
+            			PrintWriter searchOutWriter = new PrintWriter(System.out, true);
             			
             			int curExp = 0;
             			while (curExp < EXP_NUMBER)
-            			{
+            			{            				            	            
             				startTime = System.nanoTime();
                 			// do the experiment (depends on text file)
                 			// process the operations
                 			MultisetTester.processOperations(inReader, searchOutWriter, implementType);
                 			endTime = System.nanoTime();
                 			
-                			sum += (endTime - startTime);
-                			timeElapsed = (endTime - startTime) / Math.pow(10, 9);	
-                			searchOutWriter.println(String.format("Time elapsed (secs): %.10f\n", timeElapsed));
+                			timeElapsed = (endTime - startTime) / Math.pow(10, 9);
+                			recordList[curExp] = timeElapsed;
+                			filehOutWriter.println(String.format("Time elapsed (secs): %.10f", timeElapsed));
                             System.out.printf("Time elapsed (secs): %.10f\n", timeElapsed);
                 			curExp++;
             			}
             			
             			// calculate the average
-            			double average = (sum / EXP_NUMBER ) / Math.pow(10, 9);
-            			searchOutWriter.println("DataStructure:" + dataUsed + " Testing file: " + fileName);
-            			searchOutWriter.println(String.format("Average time performance (secs): %.10f\n", average));
+            			int index = 0;
+            			double sum = 0;
+            			
+            			while(index<recordList.length)
+            			{
+            				sum += recordList[index];
+            				index++;
+            			}
+            			
+            			double average = sum / EXP_NUMBER ;
+            			filehOutWriter.println("DataStructure:" + dataUsed + " Testing file: " + fileName);
+            			filehOutWriter.println(String.format("Average time performance (secs): %.10f\n", average));
             			System.out.println("DataStructure:" + dataUsed + " Testing file: " + fileName);
             			System.out.printf("Average time performance (secs): %.10f\n", average);
             		} 
@@ -126,7 +136,7 @@ public class PerformanceAnalysis
             		}
                 }
             }
-            searchOutWriter.close();
+            filehOutWriter.close();
         }
         catch (Exception e) {
             System.err.println(e.getMessage());
